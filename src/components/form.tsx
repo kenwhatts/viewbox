f7556b3b8c24df3cf4925507f92aefa98499a3b4
kendpr <119304897+RealKendpr@users.noreply.github.com>
2025-02-27 09:36:49 +0800

    added user authentication, and setup mongodb, and dashboard page - did not use react-hook-form yeat

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
 import { Inputs } from "./input";
+import { useActionState } from "react";
+import { authenticate } from "../app/lib/actions";
+import { useSearchParams } from "next/navigation";
 
 interface Inputs {
   username: string;
@@ -9,19 +12,53 @@ interface Inputs {
 export function Form() {
   const methods = useForm<Inputs>();
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/dashboard";
+  const [errorMessage, formAction, isPending] = useActionState(
+    authenticate,
+    undefined
+  );
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
 
   return (
-    <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>
-        <Inputs
-          name="username"
-          label="Username"
-          placeholder="Username"
-        ></Inputs>
-        <Inputs name="password" label="Password"></Inputs>
-        <button type="submit">Submit</button>
-      </form>
-    </FormProvider>
+    // <FormProvider {...methods}>
+    //   <form action={formAction} onSubmit={methods.handleSubmit(onSubmit)}>
+    //     <Inputs name="email" label="Username" placeholder="Username"></Inputs>
+    //     <Inputs name="password" label="Password"></Inputs>
+    //     <input type="hidden" name="redirectTo" value={callbackUrl} />
+    //     <button type="submit" aria-disabled={isPending}>
+    //       Submit
+    //     </button>
+    //     {errorMessage && <p className="text-sm text-red-500">{errorMessage}</p>}
+    //   </form>
+    // </FormProvider>
+
+    <form action={formAction}>
+      <input
+        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        id="email"
+        type="email"
+        name="email"
+        placeholder="Enter your email address"
+        required
+      />
+      <input
+        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        id="password"
+        type="password"
+        name="password"
+        placeholder="Enter password"
+        required
+        minLength={6}
+      />
+      <input type="hidden" name="redirectTo" value={callbackUrl} />
+      <button className="mt-4 w-full" aria-disabled={isPending}>
+        Log in
+      </button>
+      {errorMessage && <p className="text-sm text-red-500">{errorMessage}</p>}
+    </form>
   );
 }
