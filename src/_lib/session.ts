3828a0b0abef2cc3ed4c2a7520317f7ff2e285b0
kendpr <119304897+RealKendpr@users.noreply.github.com>
2025-03-03 03:51:47 +0800

    added middleware to redirect user depending on session auth

--- src/_lib/session.ts
+++ src/_lib/session.ts
@@ -30,3 +30,14 @@ export async function encrypt(payload: SessionPayload) {
     .setExpirationTime("7d")
     .sign(encodedKey);
 }
+
+export async function decrypt(session: string | undefined = "") {
+  try {
+    const { payload } = await jwtVerify(session, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    return payload;
+  } catch (error) {
+    console.log("Failed to verify session");
+  }
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,28 @@
-import NextAuth from "next-auth";
-import { authConfig } from "../auth.config";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { decrypt } from "./_lib/session";
 
-export default NextAuth(authConfig).auth;
+const protectedRoutes = ["/dashboard"];
+const publicRoutes = ["/login"];
+
+export default async function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const isProtected = protectedRoutes.includes(path);
+  const isPublic = publicRoutes.includes(path);
+
+  const cookie = await cookies();
+  const session = await decrypt(cookie.get("session")?.value);
+
+  if (isProtected && !session?.userId) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+
+  if (isPublic && session?.userId) {
+    return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
