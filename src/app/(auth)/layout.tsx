546eaf1f59e00f1658f1e7b2470dca90482fc415
kendpr <119304897+RealKendpr@users.noreply.github.com>
2025-03-05 03:15:53 +0800

    fix pauth pages layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,16 @@
+import { PublicHeader } from "@/_components/publicHeader";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="w-full max-w-md">{children}</div>;
+  return (
+    <>
+      <PublicHeader />
+      <main className="min-h-screen grid place-items-center">
+        <div className="w-full max-w-md">{children}</div>
+      </main>
+    </>
+  );
 }
