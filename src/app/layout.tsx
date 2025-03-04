97398f57be88031e04db28f8322f003455c15297
kendpr <119304897+RealKendpr@users.noreply.github.com>
2025-03-04 10:57:07 +0800

    added "logo"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "@/globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "One Page",
@@ -13,6 +14,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-base-100">
+        <header className="absolute p-2.5">
+          <Link className="text-2xl font-bold text-accent" href={"/"}>
+            One Page
+          </Link>
+        </header>
         <main className="hero-content min-h-screen">{children}</main>
       </body>
     </html>
