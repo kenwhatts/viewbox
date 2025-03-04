fc7a2a1731ac1e199f7599be16c4aa44f1c61b8d
kendpr <119304897+RealKendpr@users.noreply.github.com>
2025-03-05 07:57:52 +0800

    table component

---
+++ src/app/dashboard/_components/pageTable.tsx
@@ -0,0 +1,51 @@
+import mockPageData from '@/_mockData/document.json'
+import Image from 'next/image'
+
+export function PageTable() {
+  return (
+    <div className='overflow-x-auto shadow-md border'>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th className='hidden md:table-cell'></th>
+            <th>Name</th>
+            <th>created at</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th className='hidden md:table-cell'>
+              <Image
+                className='size-[64px] object-cover'
+                src={mockPageData.pageIcon}
+                alt=''
+                width={100}
+                height={100}
+              />
+            </th>
+            <th>
+              <div>{mockPageData.name}</div>
+            </th>
+            <th>{mockPageData.createdAt}</th>
+            <th>
+              <button className='btn-ghost btn btn-xs'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width={32}
+                  height={32}
+                  viewBox='0 0 32 32'>
+                  <path
+                    fill='none'
+                    stroke='currentColor'
+                    strokeWidth={2}
+                    d='M8 17a1 1 0 1 0 0-2a1 1 0 0 0 0 2Zm8 0a1 1 0 1 0 0-2a1 1 0 0 0 0 2Zm8 0a1 1 0 1 0 0-2a1 1 0 0 0 0 2Z'></path>
+                </svg>
+              </button>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,59 +1,15 @@
-"use client";
-import mockPageData from "@/_mockData/document.json";
-import Image from "next/image";
+'use client'
+import { PageTable } from './_components/pageTable'
 
 export default function Dashboard() {
   return (
     <div>
-      <div className="mb-5">
-        <h1 className="text-2xl">Dashboard</h1>
-      </div>
-      <div className="overflow-x-auto">
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="hidden md:table-cell"></th>
-              <th>Name</th>
-              <th>created at</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th className="hidden md:table-cell">
-                <Image
-                  className="size-[64px] object-cover"
-                  src={mockPageData.pageIcon}
-                  alt=""
-                  width={100}
-                  height={100}
-                />
-              </th>
-              <th>
-                <div>{mockPageData.name}</div>
-              </th>
-              <th>{mockPageData.createdAt}</th>
-              <th>
-                <button className="btn-ghost btn btn-xs">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={32}
-                    height={32}
-                    viewBox="0 0 32 32"
-                  >
-                    <path
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      d="M8 17a1 1 0 1 0 0-2a1 1 0 0 0 0 2Zm8 0a1 1 0 1 0 0-2a1 1 0 0 0 0 2Zm8 0a1 1 0 1 0 0-2a1 1 0 0 0 0 2Z"
-                    ></path>
-                  </svg>
-                </button>
-              </th>
-            </tr>
-          </tbody>
-        </table>
+      <div>
+        <div className='flex flex-row-reverse mb-4'>
+          <button className='btn btn-secondary'>Create Page</button>
+        </div>
+        <PageTable />
       </div>
     </div>
-  );
+  )
 }
