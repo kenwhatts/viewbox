58e68d03485e12031494922a11bf3beee4c87160
kendpr <119304897+RealKendpr@users.noreply.github.com>
2025-03-05 22:07:53 +0800

    protect subpages of protected routes as well

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,27 @@
-import { cookies } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
-import { decrypt } from "./_lib/session";
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+import { decrypt } from './_lib/session';
 
-const protectedRoutes = ["/dashboard"];
-const publicRoutes = ["/login", "/register"];
+const protectedRoutes = ['/dashboard'];
+const publicRoutes = ['/login', '/register'];
 
 export default async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  const isProtected = protectedRoutes.includes(pathname);
+  const isProtected = protectedRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
   const isPublic = publicRoutes.includes(pathname);
 
-  const cookie = (await cookies()).get("session")?.value;
+  const cookie = (await cookies()).get('session')?.value;
   const session = await decrypt(cookie, pathname);
 
   if (isProtected && !session?.userId) {
-    return NextResponse.redirect(new URL("/login", request.nextUrl));
+    return NextResponse.redirect(new URL('/login', request.nextUrl));
   }
 
   if (isPublic && session?.userId) {
-    return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
+    return NextResponse.redirect(new URL('/dashboard', request.nextUrl));
   }
 
   return NextResponse.next();
@@ -27,5 +29,5 @@ export default async function middleware(request: NextRequest) {
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ["/dashboard/:path*", "/login", "/register"],
+  matcher: ['/dashboard/:path*', '/login', '/register']
 };
