f47a9822da5bcf00b4c24339a205325ed6c43189
kendpr <119304897+RealKendpr@users.noreply.github.com>
2025-03-08 04:47:29 +0800

    - each object must be checked before proceeding, this is to prevent app from crashing when websites[x] has a present error and the user tries to add another website[y]
    - reproduce: submit an empty form, then add another website fields
    
    - when this happens, `errors.websites[index][arrayFieldName]` run but it doesnt know that new `errors.websites[index]` actually has no errors thus undefined.

--- src/app/dashboard/create/_components/input.tsx
+++ src/app/dashboard/create/_components/input.tsx
@@ -31,44 +31,63 @@ export function Input({
         : undefined
   };
 
-  useEffect(() => {
-    console.log(errors.websites && errors.websites);
-  });
+  // useEffect(() => {
+  //   console.log("arrayLog:", errors.websites && errors.websites);
 
-  // const ErrorAlert = () => {
   //   const arrayFieldName = name.substring(name.lastIndexOf(".") + 1);
 
-  //   // errors for fields inside the  useFieldArray
   //   if (index !== undefined) {
-  //     if (
-  //       Array.isArray(errors.websites) &&
-  //       errors.websites[index][arrayFieldName]
-  //     ) {
-  //       const { setOpenAccordion } = useAccordionContext();
-
-  //       setOpenAccordion(true);
-  //       return (
-  //         // errors.websites[index] is not an array or is undefined on first page load since there is no errors yet, so, erros.websites needs to be checked as an array before checking which field has an error, else the application will crash.
-  //         // this is the way I check for field errors because I cant find a documentation/guide on how to retrieve errors from useFieldArray
-  //         // this also took me hours :/
-  //         <RequiredAlert
-  //           errorMsg={`Please enter a valid ${
-  //             type === "url" ? "URL" : label.toLowerCase()
-  //           }`}
-  //         />
-  //       );
-  //     }
-  //   }
-  //   // errors for normal fields
-  //   else if (errors[name])
-  //     return (
-  //       <RequiredAlert
-  //         errorMsg={`Please enter a valid ${
-  //           type === "url" ? "URL" : label.toLowerCase()
-  //         }`}
-  //       />
+  //     console.log(
+  //       "indexLog:",
+  //       Array.isArray(errors.websites) && errors.websites[index]
   //     );
-  // };
+
+  //     console.log(
+  //       "objLog",
+  //       Array.isArray(errors.websites) &&
+  //         errors.websites[index] &&
+  //         errors.websites[index][arrayFieldName]
+  //     );
+  //   }
+  // });
+
+  const ErrorAlert = () => {
+    const arrayFieldName = name.substring(name.lastIndexOf(".") + 1);
+
+    // errors for fields inside the  useFieldArray
+    if (index !== undefined) {
+      if (
+        // each object must be checked before proceeding, this is to prevent app from crashing when websites[x] has a present error and the user tries to add another website[y]
+        // reproduce: submit an empty form, then add another website fields
+        Array.isArray(errors.websites) &&
+        errors.websites[index] &&
+        errors.websites[index][arrayFieldName]
+      ) {
+        const { setOpenAccordion } = useAccordionContext();
+
+        setOpenAccordion(true);
+        return (
+          // errors.websites[index] is not an array or is undefined on first page load since there is no errors yet, so, erros.websites needs to be checked as an array before checking which field has an error, else the application will crash.
+          // this is the way I check for field errors because I cant find a documentation/guide on how to retrieve errors from useFieldArray
+          // this also took me hours :/
+          <RequiredAlert
+            errorMsg={`Please enter a valid ${
+              type === "url" ? "URL" : label.toLowerCase()
+            }`}
+          />
+        );
+      }
+    }
+    // errors for normal fields
+    else if (errors[name])
+      return (
+        <RequiredAlert
+          errorMsg={`Please enter a valid ${
+            type === "url" ? "URL" : label.toLowerCase()
+          }`}
+        />
+      );
+  };
 
   return (
     <div>
@@ -82,7 +101,7 @@ export function Input({
         placeholder={placeholder}
         {...register(name, validationRule)}
       />
-      {/* <ErrorAlert /> */}
+      <ErrorAlert />
     </div>
   );
 }
