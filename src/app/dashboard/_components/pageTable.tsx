a4fc7df6841205163cd9ee72a67dc17128d0b9d7
kendpr <119304897+RealKendpr@users.noreply.github.com>
2025-03-10 18:45:18 +0800

    display list of pages

--- src/app/dashboard/_components/pageTable.tsx
+++ src/app/dashboard/_components/pageTable.tsx
@@ -1,9 +1,20 @@
-import mockPageData from "@/_mockData/document.json";
 import Image from "next/image";
+import { getPages } from "../getPages";
 
-export function PageTable() {
+interface PagesResultType {
+  pageName: string;
+  pageIcon: string;
+  createdAt: Date;
+}
+
+export async function PageTable() {
+  const pages: any = await getPages();
+  const pagesResult: PagesResultType[] = await pages;
+
+  const createdDate = (date: Date) => new Date(date).toLocaleDateString();
+
   return (
-    <div className="overflow-x-auto shadow-md border">
+    <div className="overflow-x-auto border shadow-md">
       <table className="table">
         <thead>
           <tr>
@@ -14,38 +25,45 @@ export function PageTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th className="hidden md:table-cell text-end">
-              <div className="rounded-xl overflow-hidden inline-block">
-                <Image
-                  className="size-[64px] object-cover"
-                  src={mockPageData.pageIcon}
-                  alt=""
-                  width={100}
-                  height={100}
-                />
-              </div>
-            </th>
-            <th>
-              <div>{mockPageData.name}</div>
-            </th>
-            <th>{mockPageData.createdAt}</th>
-            <th>
-              <button className="btn-ghost btn btn-xs">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={32}
-                  height={32}
-                  viewBox="0 0 32 32">
-                  <path
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    d="M8 17a1 1 0 1 0 0-2a1 1 0 0 0 0 2Zm8 0a1 1 0 1 0 0-2a1 1 0 0 0 0 2Zm8 0a1 1 0 1 0 0-2a1 1 0 0 0 0 2Z"></path>
-                </svg>
-              </button>
-            </th>
-          </tr>
+          {pagesResult &&
+            pagesResult.map((i, index) => (
+              <tr key={index}>
+                <th>
+                  <div>
+                    {i.pageIcon && (
+                      <Image
+                        className="size-[64px] object-cover"
+                        src={i.pageIcon}
+                        alt=""
+                        width={100}
+                        height={100}
+                      />
+                    )}
+                  </div>
+                </th>
+                <th>
+                  <div>{i.pageName}</div>
+                </th>
+                <th>{createdDate(i.createdAt)}</th>
+                <th>
+                  <button className="btn-ghost btn btn-xs">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={32}
+                      height={32}
+                      viewBox="0 0 32 32"
+                    >
+                      <path
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        d="M8 17a1 1 0 1 0 0-2a1 1 0 0 0 0 2Zm8 0a1 1 0 1 0 0-2a1 1 0 0 0 0 2Zm8 0a1 1 0 1 0 0-2a1 1 0 0 0 0 2Z"
+                      ></path>
+                    </svg>
+                  </button>
+                </th>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
