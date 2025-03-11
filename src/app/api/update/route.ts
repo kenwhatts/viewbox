1822b7d6241417fee67b1aa8d6d49f197299e162
kendpr <119304897+RealKendpr@users.noreply.github.com>
2025-03-11 22:36:07 +0800

    created an update route

---
+++ src/app/api/update/route.ts
@@ -0,0 +1,71 @@
+import { getUserData } from "@/_lib/getUserData";
+import { EditPageType, PageDocumentType } from "@/types/PageTypes";
+import { NextRequest, NextResponse } from "next/server";
+import { pageSchema } from "../_schema/pageSchema";
+import { connectDB } from "@/_lib/mongodb/mongodb";
+import PageModel from "@/_lib/mongodb/models/PageModel";
+
+export async function PUT(request: NextRequest) {
+  const pathname = request.headers.get("X-Pathname");
+  const data: EditPageType = await request.json();
+
+  if (!data || pathname == null)
+    return NextResponse.json(
+      { error: "empty request not accepted" },
+      { status: 400 },
+    );
+
+  const userData = await getUserData(pathname);
+  const userId = userData?._id;
+
+  if (!userId)
+    return NextResponse.json(
+      { error: "request is unauthenticated" },
+      { status: 401 },
+    );
+
+  const result = pageSchema.safeParse(data);
+
+  if (!result.success) {
+    return NextResponse.json(result.error);
+  }
+
+  const pageId = data._id;
+  const { pageName, pageIcon, websites } = result.data;
+
+  try {
+    await connectDB();
+
+    // must check first if there is any page document that has the user's id,
+    // and that have the requested page id
+
+    const findPage: PageDocumentType | null = await PageModel.findOne({
+      pageId,
+      userId,
+    });
+
+    if (!findPage)
+      return NextResponse.json(
+        { message: "client does not have access rights to the content" },
+        { status: 403 },
+      );
+
+    const updatedPage = await PageModel.findOneAndUpdate(
+      { pageId },
+      result.data,
+    );
+
+    await updatedPage.save();
+
+    return NextResponse.json(
+      { message: "a new resource was created" },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "internal server error" },
+      { status: 500 },
+    );
+  }
+}
