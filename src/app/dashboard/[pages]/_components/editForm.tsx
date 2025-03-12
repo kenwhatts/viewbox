0301661dbc6fa1d3130150716c9a603cadd27131
kendpr <119304897+RealKendpr@users.noreply.github.com>
2025-03-12 12:56:09 +0800

    `put` request, if the page needs to be updated

--- src/app/dashboard/[pages]/_components/editForm.tsx
+++ src/app/dashboard/[pages]/_components/editForm.tsx
@@ -2,21 +2,53 @@
 
 import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 import { InputSet } from "../../_components/inputSet";
-import { EditPageType } from "@/types/PageTypes";
+import { EditPageType, FormPageType } from "@/types/PageTypes";
+import { useState } from "react";
+import { getPathname } from "../../create/_utils/getPathname";
 
 export function EditForm({ pageDetails }: { pageDetails: string | null }) {
   const pageDetailsResult: EditPageType =
     pageDetails && JSON.parse(pageDetails);
 
-  const methods = useForm<EditPageType>({
-    defaultValues: {
-      pageName: pageDetailsResult?.pageName,
-      pageIcon: pageDetailsResult?.pageIcon,
-      websites: pageDetailsResult?.websites,
-    },
+  const formDefaultValues: FormPageType = {
+    pageName: pageDetailsResult?.pageName,
+    pageIcon: pageDetailsResult?.pageIcon,
+    websites: pageDetailsResult?.websites,
+  };
+
+  const methods = useForm<FormPageType>({
+    defaultValues: formDefaultValues,
   });
 
-  const onSubmit: SubmitHandler<EditPageType> = () => {};
+  const [isSame, setIsSame] = useState<boolean>(false);
+
+  const onSubmit: SubmitHandler<FormPageType> = async (formData) => {
+    // check if submitted data and current value is the same,
+    // before making a proceeding to avoid unnecessary request
+    // and show an alert about it
+    if (JSON.stringify(formData) === JSON.stringify(formDefaultValues)) {
+      setIsSame(true);
+      return null;
+    }
+    setIsSame(false);
+
+    const response = await fetch("/api/update", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Pathname": await getPathname(),
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      console.log(result);
+    }
+
+    console.log(result);
+  };
 
   return (
     <FormProvider {...methods}>
