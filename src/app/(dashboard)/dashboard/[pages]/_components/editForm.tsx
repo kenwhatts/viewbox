6ea67ac41f3653ad863e93b25285a8d2838bfcf3
kendpr <119304897+RealKendpr@users.noreply.github.com>
2025-03-18 22:51:30 +0800

    Add: website list on the edit form

--- src/app/(dashboard)/dashboard/[pages]/_components/editForm.tsx
+++ src/app/(dashboard)/dashboard/[pages]/_components/editForm.tsx
@@ -2,41 +2,57 @@
 
 import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 import { InputSet } from "../../_components/inputSet";
-import { EditPageType, PageType } from "@/types/PageTypes";
+import { EditPageType, PageType, WebsiteType } from "@/types/PageTypes";
 import { useState } from "react";
 import { deletePage } from "../deletePage";
 import { useRouter } from "next/navigation";
+import dynamic from "next/dynamic";
+const AddWebsite = dynamic(() => import("@dashboard/_components/addWebsite"));
 
 export function EditForm({ pageDetails }: { pageDetails: string | null }) {
-  const router = useRouter();
-  const [isSame, setIsSame] = useState<boolean>(false);
-
   const pageDetailsResult: EditPageType =
     pageDetails && JSON.parse(pageDetails);
-
-  const formDefaultValues: PageType = {
-    pageIcon: pageDetailsResult?.pageIcon,
-    pageName: pageDetailsResult?.pageName,
-    pageDescription: pageDetailsResult.pageDescription,
-    websites: pageDetailsResult?.websites,
-  };
 
   const extendedKeys = {
     _id: pageDetailsResult._id,
     createdAt: pageDetailsResult.createdAt,
   };
+  const formDefaultValues = {
+    pageIcon: pageDetailsResult?.pageIcon,
+    pageName: pageDetailsResult?.pageName,
+    pageDescription: pageDetailsResult.pageDescription,
+  }; // websites must be handled by a useState,
+  // because it was not exclusively a part of the form when data
+  // was created/submitted to server
+  const [website, setWebsite] = useState<WebsiteType[]>(
+    pageDetailsResult.websites,
+  );
+
+  const router = useRouter();
+  const [isSame, setIsSame] = useState<boolean>(false);
 
   const methods = useForm<PageType>({
     defaultValues: formDefaultValues,
   });
 
   const onSubmit: SubmitHandler<PageType> = async (formData) => {
+    const newFormData: PageType = {
+      ...formData,
+      websites: website,
+    };
+
     // check if submitted data and current value is the same,
-    // before making a proceeding to avoid unnecessary request
+    // before proceeding to avoid unnecessary request
     // and show an alert about it
-    if (JSON.stringify(formData) === JSON.stringify(formDefaultValues)) {
+    if (
+      JSON.stringify(newFormData) ===
+      JSON.stringify({
+        ...formDefaultValues,
+        websites: pageDetailsResult.websites,
+      })
+    ) {
       setIsSame(true);
-      return null;
+      return;
     }
     setIsSame(false);
 
@@ -45,22 +61,23 @@ export function EditForm({ pageDetails }: { pageDetails: string | null }) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ ...formData, ...extendedKeys }),
+      body: JSON.stringify({ ...newFormData, ...extendedKeys }),
     });
 
-    const result = await response.json();
-
     if (!response.ok) {
-      console.log(result);
+      return;
     }
-
-    console.log(result);
   };
 
   return (
     <FormProvider {...methods}>
       <form className="max-w-md" onSubmit={methods.handleSubmit(onSubmit)}>
         <InputSet />
+        <AddWebsite
+          website={website}
+          setWebsite={setWebsite}
+          methods={methods}
+        />
         <button className="btn btn-primary w-full">Update</button>
         <button
           className="btn btn-warning mt-[8.25px] w-full"
--- src/app/(dashboard)/dashboard/create/_components/createForm.tsx
+++ src/app/(dashboard)/dashboard/create/_components/createForm.tsx
@@ -43,6 +43,7 @@ export function CreateForm() {
         setLoading(false);
         setIsDuplicate((isDuplicate) => !isDuplicate);
       }
+      setLoading(false);
       return;
     }
     setLoading(false);
