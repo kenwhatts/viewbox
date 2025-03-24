13599e198e1e852a19b7cdebc48debfd402828b4
kendpr <119304897+RealKendpr@users.noreply.github.com>
2025-03-24 21:45:34 +0800

    feat: fallback profile image and fix header avatar not circle
    a fallback will be shown if there is no upload image to serve as profile

---
+++ src/_components/userAvatar.tsx
@@ -0,0 +1,32 @@
+import { createAvatar } from "@dicebear/core";
+import { useMemo } from "react";
+import Image from "next/image";
+
+export function UserAvatar({
+  username,
+  size,
+  className,
+  style,
+}: {
+  username: string;
+  size: number;
+  className?: string;
+  style: any;
+}) {
+  const avatar = useMemo(() => {
+    return createAvatar(style, {
+      seed: username,
+      size: size,
+    }).toDataUri();
+  }, []);
+
+  return (
+    <Image
+      className={className}
+      src={avatar}
+      alt=""
+      width={size}
+      height={size}
+    />
+  );
+}
--- src/app/(pages)/[pages]/_components/pageDetails.tsx
+++ src/app/(pages)/[pages]/_components/pageDetails.tsx
@@ -1,19 +1,28 @@
 import { PageType } from "@/types/PageTypes";
 import Image from "next/image";
 import { favicon } from "@/app/_utils/getFavicon";
+import { UserAvatar } from "@/_components/userAvatar";
+import { funEmoji } from "@dicebear/collection";
 
 export function PageDetails({ page }: { page: PageType }) {
   return (
     <div className="bg-base-100 mx-auto min-h-[60%] w-[calc(80%+12px)] rounded-lg px-4 py-10">
       <div className="mb-4">
-        <div className="mx-auto size-20">
-          {page.pageIcon && (
+        <div className="mx-auto size-21">
+          {page.pageIcon ? (
             <Image
               className="size-20 rounded-full object-cover"
               src={page.pageIcon}
               width={80}
               height={80}
               alt=""
+            />
+          ) : (
+            <UserAvatar
+              className="size-20 rounded-full object-cover"
+              username={page.pageName}
+              size={80}
+              style={funEmoji}
             />
           )}
         </div>
