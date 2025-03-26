a59e53a482fa99fb60035f6165d27fdd3bcf5a87
Ken <119304897+RealKendpr@users.noreply.github.com>
2025-03-26 16:38:19 +0800

    style: different layouts for the page

--- src/app/(pages)/[pages]/_components/pageDetails.tsx
+++ src/app/(pages)/[pages]/_components/pageDetails.tsx
@@ -4,24 +4,25 @@ import { favicon } from "@/app/_utils/getFavicon";
 import { UserAvatar } from "@/_components/userAvatar";
 import { funEmoji } from "@dicebear/collection";
 import { getOptions } from "@/_lib/getPageConfig";
+// import "@[pages]/_components/default.css";
+import "@[pages]/_components/bentoLayout.css";
 
 export async function PageDetails({
   page,
   slug,
 }: {
   page: PageType;
-
   slug: string;
 }) {
   const options = await getOptions(slug);
 
   return (
-    <div className="bg-base-100 mx-auto min-h-[60%] w-[calc(80%+12px)] rounded-lg px-4 py-10">
+    <div className={`bg-base-100 dimension layout rounded-lg px-4 py-10`}>
       <div className="mb-4">
-        <div className="mx-auto size-21">
+        <div className="image mx-auto size-21">
           {page.pageIcon ? (
             <Image
-              className="size-20 rounded-full object-cover"
+              className="size-20 object-cover"
               src={page.pageIcon}
               width={80}
               height={80}
@@ -29,19 +30,21 @@ export async function PageDetails({
             />
           ) : (
             <UserAvatar
-              className="size-20 rounded-full object-cover"
+              className="size-20 object-cover"
               username={page.pageName}
               size={80}
               style={funEmoji}
             />
           )}
         </div>
-        <h1 className="mt-4 mb-5 text-center text-xl font-semibold">
-          {page.pageName}
-        </h1>
-        <p className="text-center text-sm">{page.pageDescription}</p>
+        <div>
+          <h1 className="mt-4 mb-5 text-center text-xl font-semibold">
+            {page.pageName}
+          </h1>
+          <p className="text-center text-sm">{page.pageDescription}</p>
+        </div>
       </div>
-      <ul className="grid gap-y-3">
+      <ul className="">
         {page.links.map((item, index) => (
           <li key={index}>
             <a
