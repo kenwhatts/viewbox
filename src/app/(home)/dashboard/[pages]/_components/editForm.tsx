f767c95d8b4fcad06a5389d57599d2ea6c9768eb
Ken <119304897+RealKendpr@users.noreply.github.com>
2025-03-28 18:37:30 +0800

    feat: show updated modal, but dont make a request
    show the the updated modal if the user attempts to update an unchanged form, but dont make a request. this is to reduce the amount of modals

--- src/app/(home)/dashboard/[pages]/_components/editForm.tsx
+++ src/app/(home)/dashboard/[pages]/_components/editForm.tsx
@@ -31,7 +31,6 @@ export function EditForm({ pageDetails }: { pageDetails: EditPageType }) {
   const [links, setLinks] = useState<LinkType[]>(pageDetails.links);
 
   const router = useRouter();
-  const [isSame, setIsSame] = useState<boolean>(false);
   const [isDuplicate, setIsDuplicate] = useState<boolean>(false);
   const [updated, setUpdated] = useState<boolean>(false);
   const [linkRequired, setLinkRequired] = useState<boolean>(false);
@@ -56,10 +55,9 @@ export function EditForm({ pageDetails }: { pageDetails: EditPageType }) {
         links: pageDetails.links,
       })
     ) {
-      setIsSame(true);
+      setUpdated(true);
       return;
     }
-    setIsSame(false);
 
     if (links.length === 0) {
       setLinkRequired(true);
@@ -88,32 +86,16 @@ export function EditForm({ pageDetails }: { pageDetails: EditPageType }) {
 
   return (
     <>
+      <FormProvider {...methods}>
+        <form className="max-w-md" onSubmit={methods.handleSubmit(onSubmit)}>
+          <InputSet />
+          <AddLink links={links} setLinks={setLinks} />
+          <SubmitBtn loading={methods.formState.isSubmitting} name="Update" />
+          <DeletePageBtn pageId={extendedKeys._id} router={router} />
+        </form>
+      </FormProvider>
       <Modal isOpen={linkRequired} setIsOpen={setLinkRequired}>
         <p>⚠️ At least one link is required</p>
-      </Modal>
-      <Modal isOpen={isSame} setIsOpen={setIsSame}>
-        <p className="text-info flex items-center gap-x-2 font-semibold">
-          <span>
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM1.82707 7.49972C1.82707 4.36671 4.36689 1.82689 7.49991 1.82689C10.6329 1.82689 13.1727 4.36671 13.1727 7.49972C13.1727 10.6327 10.6329 13.1726 7.49991 13.1726C4.36689 13.1726 1.82707 10.6327 1.82707 7.49972ZM8.24992 4.49999C8.24992 4.9142 7.91413 5.24999 7.49992 5.24999C7.08571 5.24999 6.74992 4.9142 6.74992 4.49999C6.74992 4.08577 7.08571 3.74999 7.49992 3.74999C7.91413 3.74999 8.24992 4.08577 8.24992 4.49999ZM6.00003 5.99999H6.50003H7.50003C7.77618 5.99999 8.00003 6.22384 8.00003 6.49999V9.99999H8.50003H9.00003V11H8.50003H7.50003H6.50003H6.00003V9.99999H6.50003H7.00003V6.99999H6.50003H6.00003V5.99999Z"
-                fill="currentColor"
-                fillRule="evenodd"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </span>
-          <span>No changes detected</span>
-        </p>
-        <p className="py-4 text-sm">
-          You don't need to update, because you didn't change anything.
-        </p>
       </Modal>
       <Modal isOpen={isDuplicate} setIsOpen={setIsDuplicate}>
         <p className="font-semibold">⚠️ Page already exist</p>
@@ -143,14 +125,6 @@ export function EditForm({ pageDetails }: { pageDetails: EditPageType }) {
           <span>Updated successfully</span>
         </p>
       </Modal>
-      <FormProvider {...methods}>
-        <form className="max-w-md" onSubmit={methods.handleSubmit(onSubmit)}>
-          <InputSet />
-          <AddLink links={links} setLinks={setLinks} />
-          <SubmitBtn loading={methods.formState.isSubmitting} name="Update" />
-          <DeletePageBtn pageId={extendedKeys._id} router={router} />
-        </form>
-      </FormProvider>
     </>
   );
 }
