f1d52bd22f0cc15d4a379b0a53c8ff2211631fc7
Ken <119304897+RealKendpr@users.noreply.github.com>
2025-04-04 08:05:30 +0800

    fix: link hover color for colored cards

--- src/app/(pages)/[pages]/_components/pageDetails.tsx
+++ src/app/(pages)/[pages]/_components/pageDetails.tsx
@@ -16,47 +16,82 @@ export async function PageDetails({
   const styles = await getStyles(slug);
 
   return (
-    <div
-      className="card bg-base-100 rounded-lg"
-      style={{ background: styles?.cardColor, color: styles?.textColor }}
-    >
-      <div className="card-header">
-        <div className="profile-image">
-          {page.pageIcon ? (
-            <Image
-              className="size-20 object-cover"
-              src={page.pageIcon}
-              width={80}
-              height={80}
-              alt=""
-            />
-          ) : (
-            <UserAvatar username={page.pageName} size={150} style={funEmoji} />
-          )}
+    <>
+      <style>{`
+        .link-list {
+          & li {
+            background: ${styles?.linkBackground};
+            color: ${styles?.textColor};
+          }
+
+          & .colored-card {
+            position: relative;
+
+            &::after {
+              content: '';
+              position: absolute;
+              top: 0;
+              bottom: 0;
+              right: 0;
+              left: 0;
+              }
+
+            &:hover::after {
+              background: ${styles?.cardColor};
+              filter: invert(1);
+            }
+
+            &:hover {
+              color: ${styles?.cardColor};
+            }
+          }
+        }
+      `}</style>
+      <div
+        className="card bg-base-100 rounded-lg"
+        style={{ background: styles?.cardColor, color: styles?.textColor }}
+      >
+        <div className="card-header">
+          <div className="profile-image">
+            {page.pageIcon ? (
+              <Image
+                className="size-20 object-cover"
+                src={page.pageIcon}
+                width={80}
+                height={80}
+                alt=""
+              />
+            ) : (
+              <UserAvatar
+                username={page.pageName}
+                size={150}
+                style={funEmoji}
+              />
+            )}
+          </div>
+          <div className="card-copy">
+            <h1 className="text-xl font-semibold">{page.pageName}</h1>
+            <p className="text-sm">{page.pageDescription}</p>
+          </div>
         </div>
-        <div className="card-copy">
-          <h1 className="text-xl font-semibold">{page.pageName}</h1>
-          <p className="text-sm">{page.pageDescription}</p>
-        </div>
-      </div>
-      <ul className="link-list">
-        {page.links.map((item, index) => (
-          <li key={index}>
-            <a
-              className="btn btn-soft btn-block hover:!bg-amber-50/15"
-              style={{
-                background: styles?.linkBackground,
-                color: styles?.textColor,
-              }}
-              href={item.linkUrl}
-              target={options?.newTab ? "_blank" : "_self"}
+        <ul className="link-list">
+          {page.links.map((item, index) => (
+            <li
+              className={`btn btn-soft btn-block overflow-hidden bg-transparent px-0 transition-all duration-300 ${styles?.cardColor != "transparent" && styles?.linkBackground == "transparent" ? "colored-card after:transition-all after:duration-300" : "hover:scale-105 hover:bg-amber-50/15"}`}
+              key={index}
             >
-              {/* <img src={favicon(item.linkUrl)} width={24} height={24} alt="" /> */}
-              <span>{item.linkName}</span>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+              <a
+                className="grid size-full place-items-center transition-all duration-300"
+                href={item.linkUrl}
+                target={options?.newTab ? "_blank" : "_self"}
+              >
+                {/* <img src={favicon(item.linkUrl)} width={24} height={24} alt="" /> */}
+                <span className="z-10">{item.linkName}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
