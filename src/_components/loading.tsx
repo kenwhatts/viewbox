8677707706addaeeef22b76875e938a0403d1185
Ken <119304897+RealKendpr@users.noreply.github.com>
2025-04-12 16:19:09 +0800

    style: replaced loading spinner in `/dashboard` with a skeleton

--- src/_components/loading.tsx
+++ src/_components/loading.tsx
@@ -5,3 +5,26 @@ export function LoadingSpinner() {
     </div>
   );
 }
+
+export function PageListSkeleton({ count }: { count: number }) {
+  return (
+    <tbody>
+      {Array.from({ length: count }).map((_, index) => (
+        <tr key={index}>
+          <td className="hidden text-end md:table-cell">
+            <div className="skeleton inline-block h-[64px] w-[64px]"></div>
+          </td>
+          <td>
+            <div className="skeleton h-5 w-[10ch]"></div>
+          </td>
+          <td className="hidden sm:table-cell">
+            <div className="skeleton h-5 w-[10ch]"></div>
+          </td>
+          <td>
+            <div className="skeleton mx-auto h-6 w-[50px]"></div>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  );
+}
