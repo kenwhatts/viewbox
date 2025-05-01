2a648420a2af4ba7dca028067f61fc500c9b2f9f
Ken <119304897+RealKendpr@users.noreply.github.com>
2025-05-01 17:29:52 +0800

    BREAKING CHANGE: replaced authJs `auth()` with better auth's `useSession` for getting user data

--- src/_lib/getUserData.ts
+++ src/_lib/getUserData.ts
@@ -1,15 +1,26 @@
 import "server-only";
 import { cache } from "react";
-import { auth } from "@/auth";
+import { auth } from "@/app/_lib/auth";
 import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+
+const getSession = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  return session;
+};
 
 export const getUserId = cache(async () => {
-  const user = (await auth())?.user;
+  const session = await getSession();
 
-  if (!user) return redirect("/login");
-  else return user.id;
+  const userId = session?.user.id;
+
+  if (!session) return redirect("/login");
+  else return userId;
 });
 
-export const getUserImage = async () => (await auth())?.user?.image;
+export const getUserImage = async () => (await getSession())?.user.image;
 
-export const hasSession = async () => ((await auth()) ? true : false);
+export const hasSession = async () =>
+  (await getSession()) !== null ? true : false;
