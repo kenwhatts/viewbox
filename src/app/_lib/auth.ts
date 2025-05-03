11f3ac2ad9af4dceb9a99181bf36849d8f182cba
Ken <119304897+RealKendpr@users.noreply.github.com>
2025-05-03 18:28:36 +0800

    style(file name): renamed deletePage to deleteManyPages

--- src/app/_lib/auth.ts
+++ src/app/_lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import { deleteManyPages } from "../api/_utils/deletePage";
+import { deleteManyPages } from "../api/_utils/deleteManyPages";
 
 const client = new MongoClient(process.env.MONGODB_URI as string);
 const db = client.db();
